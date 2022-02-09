98b3eec3257bae43f9e8c92d3dd1a6374b22ec64
vivekbangare <vivek.bangare2492@gmail.com>
2022-02-09 23:43:07 +0530

    added duplicate code

--- calculator.go
+++ calculator.go
@@ -1,9 +1,13 @@
 package calculator
 
 func Add(x, y int) int {
-  return x + y
+	return x + y
 }
 
 func Subtract(x, y int) int {
-  return x - y
+	return x - y
+}
+
+func Add1(x, y int) int {
+	return x + y
 }
